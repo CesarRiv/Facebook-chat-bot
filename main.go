3bd96600c13a533b89b45cf2cfa02b98bad0a3a8
Cesar Rivera <cesarrivera@Cesars-MBP.attlocal.net>
2023-08-19 16:17:35 -0500

    changes made to the project

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/cdipaolo/sentiment"
 )
 
-
 const (
 	// VERIFY_TOKEN use to verify the incoming request
 	VERIFY_TOKEN = "12345"
@@ -22,7 +21,6 @@ const (
 	// GRAPHQL_URL is a base URL v12.0 for Messenger API
 	GRAPHQL_URL = "https://graph.facebook.com/v12.0"
 )
-
 // Message data structure for message event
 type Message struct {
 	Object string `json:"object"`
@@ -44,7 +42,6 @@ type Message struct {
 		} `json:"messaging"`
 	} `json:"entry"`
 }
-
 // SendMessage data structure for send message
 type SendMessage struct {
 	Recipient struct {
@@ -54,7 +51,6 @@ type SendMessage struct {
 		Text string `json:"text"`
 	} `json:"message"`
 }
-
 // webhook is a handler for Webhook server
 func webhook(w http.ResponseWriter, r *http.Request) {
 	// return all with status code 200
@@ -65,7 +61,6 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 		log.Printf("invalid method: not get or post")
 		return
 	}
-
 	// if the method of request is GET
 	if r.Method == http.MethodGet {
 		// read token from query parameter
@@ -76,22 +71,18 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 			log.Printf("invalid verification token: %s", verifyToken)
 			return
 		}
-
 		// write string from challenge query parameter
 		if _, err := w.Write([]byte(r.URL.Query().Get("hub.challenge"))); err != nil {
 			log.Printf("failed to write response body: %v", err)
 		}
-
 		return
 	}
-
 	// ready body in the request
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("failed to read body: %v", err)
 		return
 	}
-
 	// initiate Message data structure to message variable
 	// unmarshal []byte data into message
 	var message Message
@@ -99,88 +90,30 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Failed to unmarshal body: %v", err)
 		return
 	}
-
 	textMessage := message.Entry[0].Messaging[0].Message.Text
-
-	// send message to end-user
+	sendResponseMessage(message.Entry[0].Messaging[0].Sender.ID, textMessage)
+}
+func sendResponseMessage(senderID, message string) {
 	sentimentModel, err := sentiment.Restore()
-    if err != nil {
-        panic(err)
-    }
-	results := sentimentModel.SentimentAnalysis(textMessage, sentiment.English)
+	if err != nil {
+		log.Printf("Error initializing sentiment model: %v", err)
+		return
+	}
 
+	results := sentimentModel.SentimentAnalysis(message, sentiment.English)
+	responseMessage := ""
 
 	if results.Score > 0 {
-		err = sendMessage(message.Entry[0].Messaging[0].Sender.ID, "Positive")
-		if err != nil {
-			log.Printf("failed to send message: %v", err)
-		}
-	} else{
-		err = sendMessage(message.Entry[0].Messaging[0].Sender.ID, "Negative")
-		if err != nil {
-			log.Printf("failed to send message: %v", err)
-		}
-	}
-	/*
-	err = sendMessage(message.Entry[0].Messaging[0].Sender.ID, "Automatic Reply")
-	if err != nil {
-		log.Printf("failed to send message: %v", err)
-	}
-	*/
-
-	/*
-	// Perform sentiment analysis on the text message
-	sentimentResult := model.SentimentAnalysis(textMessage, sentiment.English)
-	if sentimentResult != nil {
-		log.Printf("Error getting sentiment analysis : %v", sentimentResult)
-		return
-	}
-
-	if sentimentResult.Score == 0 {
-		err = sendMessage(message.Entry[0].Messaging[0].Sender.ID, "Neuteral")
-		if err != nil {
-			log.Printf("failed to send message: %v", err)
-		}
-	} else if sentimentResult.Score > 0 {
-		err = sendMessage(message.Entry[0].Messaging[0].Sender.ID, "Positive")
-		if err != nil {
-			log.Printf("failed to send message: %v", err)
-		}
+		responseMessage = "Glad to hear you had a positive experience with our product!"
+	} else if results.Score == 0 {
+		responseMessage = "Thanks for the feedback!"
 	} else {
-		err = sendMessage(message.Entry[0].Messaging[0].Sender.ID, "Negative")
-		if err != nil {
-			log.Printf("failed to send message: %v", err)
-		}
-	}
-	*/
-
-	// Determine the sentiment label based on the sentiment score
-	/*
-	var sentimentLabel string
-	if sentimentResult.Score == 0 {
-		sentimentLabel = "Neutral"
-	} else if sentimentResult.Score > 0 {
-		sentimentLabel = "Positive"
-	} else {
-		sentimentLabel = "Negative"
+		responseMessage = "Sorry to hear your experience wasn't the greatest with our product."
 	}
 
-	responseMessage := fmt.Sprintf("Your message sentiment: %s", sentimentResult)
-
-	err = sendMessage(message.Entry[0].Messaging[0].Sender.ID, responseMessage)
-	if err != nil {
+	if err := sendMessage(senderID, responseMessage); err != nil {
 		log.Printf("Failed to send message: %v", err)
 	}
-
-
-
-	if err := json.Unmarshal(body, &message); err != nil {
-		log.Printf("failed to unmarshal body: %v", err)
-		return
-	}
-
-	*/
-	return
 }
 // sendMessage sends a message to end-user
 func sendMessage(senderId, message string) error {
@@ -188,18 +121,15 @@ func sendMessage(senderId, message string) error {
 	var request SendMessage
 	request.Recipient.ID = senderId
 	request.Message.Text = message
-
 	// validate empty message
 	if len(message) == 0 {
 		return errors.New("message can't be empty")
 	}
-
 	// marshal request data
 	data, err := json.Marshal(request)
 	if err != nil {
 		return fmt.Errorf("error marshall request: %w", err)
 	}
-
 	// setup http request
 	url := fmt.Sprintf("%s/%s?access_token=%s", GRAPHQL_URL, "me/messages", ACCESS_TOKEN)
 	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
@@ -207,7 +137,6 @@ func sendMessage(senderId, message string) error {
 		return fmt.Errorf("failed wrap request: %w", err)
 	}
 	req.Header.Add("Content-Type", "application/json")
-	
 	// send http request
 	client := http.Client{}
 	res, err := client.Do(req)
@@ -215,32 +144,25 @@ func sendMessage(senderId, message string) error {
 		return fmt.Errorf("failed send request: %w", err)
 	}
 	defer res.Body.Close()
-
 	// print response
 	log.Printf("message sent successfully?\n%#v", res)
-	
 	return nil
 }
 func main() {
-
-	
 	// Read the assigned port from the environment variable
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000" // Default to port 3000 if not provided
 	}
-
 	// create the handler
 	handler := http.NewServeMux()
 	handler.HandleFunc("/", webhook)
-
 	// configure http server
 	addr := fmt.Sprintf(":%s", port)
 	srv := &http.Server{
 		Handler: handler,
 		Addr:    addr, // Use the configured port
 	}
-
 	// start http server
 	log.Printf("http server listening at %v", addr)
 	if err := srv.ListenAndServe(); err != nil {
